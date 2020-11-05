7232fae31a29297d6be2f00829f319a65af6d536
root <zupanibla@gmail.com>
2020-11-05 18:35:04 +0100

    feat: tiles and enemy pathing

--- src/render-game.js
+++ src/render-game.js
@@ -1,8 +1,12 @@
 import {mat4} from 'gl-matrix';
 import {initCuboidRenderer, renderCuboids, adjustCanvasSize, setViewMatrix} from './render-cuboids.js';
 
-import duckJson  from './assets/duck.json';
-import floorJson from './assets/floor.json';
+import duckJson      from './assets/duck.json';
+import floorJson     from './assets/floor.json';
+import tileJson      from './assets/tile.json';
+import greenTileJson from './assets/green-tile.json';
+import rockyTileJson from './assets/rocky-tile.json';
+import pathTileJson  from './assets/path-tile.json';
 
 
 export function renderGame(game) {
@@ -10,7 +14,26 @@ export function renderGame(game) {
 
 	let cuboids = [];
 
-	for (let ent of game.entities) {
+    // tiles
+    for (let y = 0; y < game.tiles.length; y++) {
+        for (let x = 0; x < game.tiles[y].length; x++) {
+            let tile = game.tiles[y][x];
+            let tileCuboids = null;
+
+            if (tile == 1) tileCuboids = cuboidsFromJson(rockyTileJson);
+            if (tile == 2) tileCuboids = cuboidsFromJson(pathTileJson);
+
+            for (let it of tileCuboids) {
+                it.x = x;
+                it.y = y;
+            }
+
+            cuboids.push(...tileCuboids);
+        }
+    }
+
+    // enemies
+	for (let ent of game.enemies) {
 		
 		// duck
 		if (ent.type == 'duck') {
@@ -25,11 +48,7 @@ export function renderGame(game) {
 
 			cuboids.push(...duckCuboids);
 		}
-
 	}
-
-	// floor
-	cuboids.push(...cuboidsFromJson(floorJson));
 
 	renderCuboids(cuboids);
 }
@@ -39,9 +58,12 @@ export function initGameRenderer() {
 	initCuboidRenderer();
 
 	// set camera
+    // NOTE transformations are applied bottom up
 	let m = mat4.create();
-	mat4.translate(m, m, [0, 0, -10]);
-	mat4.rotateX(m, m, Math.PI * (-1/4));
+	mat4.translate(m, m, [0, 6, -20]);      // camera pos
+	mat4.rotateX(m, m, Math.PI * (-1/10));  // tilt world
+    mat4.scale(m, m, [3, 3, 3]);            // tile size 3
+    mat4.translate(m, m, [-7, -5.5, 0]);    // (0, 0) tile to (-7, -5.5) pos
 	setViewMatrix(m);
 }
 
@@ -53,20 +75,20 @@ function cuboidsFromJson(modelJson) {
 		cuboids.push({
 			name: it.name,
 			x: 0, y: 0, z: 0,
-			sx: it.sx * (3/256), sy: it.sy * (3/256), sz: it.sz * (3/256),
+			sx: it.sx * 1/256, sy: it.sy * 1/256, sz: it.sz * 1/256,
 			r: it.r/255, g: it.g/255, b: it.b/255,
 			rot: 0,
-			px: -it.x * (3/256), py: -it.y * (3/256), pz: -it.z * (3/256),
+			px: -it.x * 1/256, py: -it.y * 1/256, pz: -it.z * 1/256,
 		});
 
 		if (it.mirror) {
 			cuboids.push({
 				name: it.name,
 				x: 0, y: 0, z: 0,
-				sx: it.sx * (3/256), sy: it.sy * (3/256), sz: it.sz * (3/256),
+				sx: it.sx * 1/256, sy: it.sy * 1/256, sz: it.sz * 1/256,
 				r: it.r/255, g: it.g/255, b: it.b/255,
 				rot: 0,
-				px: +it.x * (3/256), py: -it.y * (3/256), pz: -it.z * (3/256),
+				px: +it.x * 1/256, py: -it.y * 1/256, pz: -it.z * 1/256,
 			});
 		}
 	}
--- src/update-game.js
+++ src/update-game.js
@@ -1,14 +1,14 @@
 
 export function updateGame(game) {
-    for (let ent of game.entities) {
+    for (let ent of game.enemies) {
 
         // duck
         if (ent.type == 'duck') {
-            ent.pathPos += 0.15 / 60;
+            ent.pathPos += 2.0 / 60;
 
             // position on path
-            let [x,  y]  = path(ent.pathPos);
-            let [x2, y2] = path(ent.pathPos + 0.05);
+            let [x,  y]  = positionOnPath(ent.pathPos, game.path);
+            let [x2, y2] = positionOnPath(ent.pathPos + 1, game.path);
             ent.x   = x;
             ent.y   = y;
             ent.rot = Math.atan2(y2-y, x2-x) + Math.PI/2;
@@ -16,14 +16,14 @@ export function updateGame(game) {
             // jumping
             if (ent.z <= 0) {
                 ent.z  = 0;
-                ent.vz = 0.15;
+                ent.vz = 0.10;
             }
 
             // apply velocity
             ent.z  += ent.vz;
 
             // gravity
-            ent.vz -= 0.014;            
+            ent.vz -= 0.010;
         }
 
     }
@@ -31,6 +31,26 @@ export function updateGame(game) {
     game.time++;
 }
 
-function path(x) {
-    return [4 * Math.cos(x * 2 * Math.PI), 4 * Math.sin(x * 2 * Math.PI)]
+
+function positionOnPath(p, path) {
+    let prevIt = path[0];
+
+    for (let it of path) {
+        let d = Math.sqrt((it[0] - prevIt[0])*(it[0] - prevIt[0]) + (it[1] - prevIt[1])*(it[1] - prevIt[1]));
+
+        if (p - d < 0) {
+            return [
+                prevIt[0] * (1 - p/d) + it[0] * p/d,
+                prevIt[1] * (1 - p/d) + it[1] * p/d,
+            ];
+        }
+
+        prevIt = it;
+        p -= d;
+    }
+
+    return [
+        path[path.length-1][0],
+        path[path.length-1][1],
+    ];
 }
