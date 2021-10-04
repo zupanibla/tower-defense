0c894af3c2841ce35753395f9a06eb4e2c52de0b
Blush <zupanibla@gmail.com>
2021-10-04 13:26:45 +0200

    feat: add terser minify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = [{
 	// game.js
 	mode: 'development',
 	devtool: 'inline-source-map',
+    /* for production builds, comment the 2 lines above and uncomment mode: 'production' and optimization */
+    // mode: 'production',
+    // optimization: {
+    //     minimize: true,
+    //     minimizer: [new TerserPlugin()],
+    // },
 	entry: './src/game.js',
 	output: {
 		filename: 'game.js',
